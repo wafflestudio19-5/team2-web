b355ea32b55c9e01f5823ef2c1ee3631c9ccc5f7
ChrisLee02 <hdchris765@snu.ac.kr>
2021-12-04 21:08:16 +0900

    수정사항반영/App.tsx모달컨테이너 이동시켰습니다

--- src/App.tsx
+++ src/App.tsx
@@ -14,21 +14,27 @@ import ModalContainer from "./Component/ModalContainer/ModalContainer";
 function App() {
     if(로그인 안되어있으면){
         return (
-            <BrowserRouter>
-                <Routes>
-                    <Route path="/" component={LoginPage} exact/>
-                    <Navigate to={"/"}/>
-                </Routes>
-            </BrowserRouter>
+            <div>
+                <BrowserRouter>
+                    <Routes>
+                        <Route path="/" component={LoginPage} exact/>
+                        <Navigate to={"/"}/>
+                    </Routes>
+                </BrowserRouter>
+                <ModalContainer/>
+            </div>
         )
     }
 
     if(로그인 되었으면){
         return (
-            <MainPage/>
+            <div>
+                <MainPage/>
+                <ModalContainer/>
+            </div>
+
         )
     }
-    <ModalContainer/>
 }
 
 export default App;
