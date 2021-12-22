00d0fb60d0ee81a306360acb113e5016e38aaf6f
Your Name <you@example.com>
2021-12-23 02:08:39 +0900

    Sprint2_이재민_2.2

--- src/Component/MainPage/MainBlock/ProfilePage/Tweets/Tweets.tsx
+++ src/Component/MainPage/MainBlock/ProfilePage/Tweets/Tweets.tsx
@@ -18,20 +18,6 @@ const dummyData=[
     retweetNumber: '2.5k',
     likeNumber: '4.5k'
   },
-  {
-    name:'야자튀김',
-    id:'FrenchFries',
-    time: '10h',
-    profileImg: 'https://img.sbs.co.kr/newsnet/etv/upload/2014/12/11/30000443115_700.jpg',
-    text: '감.자.튀.김.조.아.감.자.튀.김.조.아.감.자.튀.김.조.아.감.자.튀.김.조.아.감.자.튀.김.조.아.감.자.튀.김.조.아.감.자.튀.김.조.아.감.자.튀.김.조.아.감.자.튀.김.조.아.감.자.튀.김.조.아.감.자.튀.김.조.아.감.자.튀.김.조.아.감.자.튀.김.조.아.감.자.튀.김.조.아.감.자.튀.김.조.아',
-    images: [
-  'http://www.dailygaewon.com/news/photo/201906/4510_5372_5434.JPG',
-  'https://src.hidoc.co.kr/image/lib/2019/3/21/20190321174958927_0.jpg',
-  ],
-    commentNumber: '100',
-    retweetNumber: '2.5k',
-    likeNumber: '4.5k'
-  }
 ];
 
 
