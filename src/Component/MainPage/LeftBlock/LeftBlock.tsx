99533773961c880fd76e04c64fcf56340cef18f9
Your Name <you@example.com>
2021-12-24 01:49:44 +0900

    design & UI setting

--- src/Component/MainPage/LeftBlock/LeftBlock.tsx
+++ src/Component/MainPage/LeftBlock/LeftBlock.tsx
@@ -2,7 +2,30 @@ import styles from './LeftBlock.module.scss';
 import React from 'react';
 
 function LeftBlock() {
-  return <div className={styles.LeftBlock}>leftblock</div>;
+  return <div className={styles.LeftBlock}>
+    <header>
+      <img/>
+    </header>
+    <div className={styles.Home}>
+      <img></img>
+      Home
+    </div>
+    <div className={styles.Explore}>
+      <img></img>
+      Explore
+    </div>
+    <div className={styles.Notifications}>
+      <img></img>
+      Notifications
+    </div>
+    <div className={styles.Profile}>
+      <img></img>
+      Profile
+    </div>
+    <button className={styles.TweetButton}>
+      Tweet
+    </button>
+  </div>;
 }
 
 export default LeftBlock;
