3c4136a41db3515256f69e674fb08c8fef4108d2
tmootu76 <tmootu76@naver.com>
2022-01-01 00:35:51 +0900

    prrtier 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,50 +1,53 @@
 import styles from './App.module.scss';
 import * as React from 'react';
-import {useNavigate, Route, BrowserRouter, Navigate, Routes} from 'react-router-dom';
+import {
+  useNavigate,
+  Route,
+  BrowserRouter,
+  Navigate,
+  Routes,
+} from 'react-router-dom';
 import MainPage from './Component/MainPage/MainPage';
 import LoginPage from './Component/LoginPage/LoginPage';
-import axios from "axios"
-import {useNetworkContext} from "./Auth/AuthContext";
-import useEffect from "react"
-import KakaoAuthRedirect from "./Auth/KakaoAuthRedirect";
+import axios from 'axios';
+import { useNetworkContext } from './Auth/AuthContext';
+import useEffect from 'react';
+import KakaoAuthRedirect from './Auth/KakaoAuthRedirect';
 function App() {
+  const networkContext = useNetworkContext();
 
-    const networkContext = useNetworkContext();
+  axios.defaults.baseURL = 'http://3.35.19.155/';
+  axios.defaults.headers.common['Authorization'] =
+    'Bearer ' + networkContext.token;
 
-    axios.defaults.baseURL =
-        "http://3.35.19.155/";
-    axios.defaults.headers.common["Authorization"] = "Bearer " + networkContext.token;
-
-    if (/*networkContext.token === "undefined" ||
-      networkContext.token === undefined*/ 0) {
-
+  if (
+    /*networkContext.token === "undefined" ||
+      networkContext.token === undefined*/ 0
+  ) {
     //로그인 안 된 경우
     return (
       <div>
         <BrowserRouter>
           <Routes>
             <Route path="/" element={<LoginPage />} />
-            <Route path={"/oauth/callback/kakao"}  element={<KakaoAuthRedirect/> } />
-              <Route
-                  path="*"
-                  element={<Navigate to="/" />}
-              />
+            <Route
+              path={'/oauth/callback/kakao'}
+              element={<KakaoAuthRedirect />}
+            />
+            <Route path="*" element={<Navigate to="/" />} />
           </Routes>
         </BrowserRouter>
       </div>
     );
-
   } else {
     //로그인 된 경우
     return (
       <div className={styles.App}>
-
-          <BrowserRouter>
-              <Routes>
-                  <Route path="/*" element={<MainPage />} />
-              </Routes>
-          </BrowserRouter>
-
+        <BrowserRouter>
+          <Routes>
+            <Route path="/*" element={<MainPage />} />
+          </Routes>
+        </BrowserRouter>
       </div>
     );
   }
--- src/Auth/AuthContext.tsx
+++ src/Auth/AuthContext.tsx
@@ -1,24 +1,32 @@
-import {useContext, createContext, useState, Dispatch} from "react";
-import React from "react"
-const networkContext = createContext <{/*kakaoApproveCode:string,setKakaoApproveCode:Dispatch<any>,*/
-    token:string, setToken:Dispatch<any>}> ({
-    token:localStorage.JWT, setToken:() =>{} /*kakaoApproveCode:'null',setKakaoApproveCode:() =>{}*/ });
+import { useContext, createContext, useState, Dispatch } from 'react';
+import React from 'react';
+const networkContext = createContext<{
+  /*kakaoApproveCode:string,setKakaoApproveCode:Dispatch<any>,*/ token: string;
+  setToken: Dispatch<any>;
+}>({
+  token: localStorage.JWT,
+  setToken: () => {} /*kakaoApproveCode:'null',setKakaoApproveCode:() =>{}*/,
+});
 
-export const NetworkContextProvider = ({ children } : {children: React.ReactNode}  ) => {
-    const [token, setToken] = useState(localStorage.JWT);
-    //const [kakaoApproveCode,setKakaoApproveCode] = useState('null');
-    return (
-        <networkContext.Provider
-            value={{
-                token,
-                setToken,
-                /*kakaoApproveCode,
+export const NetworkContextProvider = ({
+  children,
+}: {
+  children: React.ReactNode;
+}) => {
+  const [token, setToken] = useState(localStorage.JWT);
+  //const [kakaoApproveCode,setKakaoApproveCode] = useState('null');
+  return (
+    <networkContext.Provider
+      value={{
+        token,
+        setToken,
+        /*kakaoApproveCode,
                 setKakaoApproveCode*/
-            }}
-        >
-            {children}
-        </networkContext.Provider>
-    );
+      }}
+    >
+      {children}
+    </networkContext.Provider>
+  );
 };
 
 export const useNetworkContext = () => useContext(networkContext);
